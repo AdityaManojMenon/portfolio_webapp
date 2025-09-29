6ed3e958a08f99c4118a1d0a2c2bb911300527d0
Adi M <am@Adis-MacBook-Air-3.local>
2025-09-29 16:33:20 -0400

    Changed favicon to pic of me

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Aditya Menon | Data Scientist",
+  title: "Aditya Menon | AI & Data Engineer",
   description: "Personal Portfolio",
 };
 
